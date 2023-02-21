034a1e3769f2e3a63ce1e06616ad142198b5d6cf
GarRustem <rustemgarifullin.mail@gmail.com>
2023-02-21 22:51:43 +0500

    v.4

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -23,7 +23,7 @@ public class BooleanSearchEngine implements SearchEngine {
                         int pages = doc.getNumberOfPages(); // Получаем количество страниц для каждого файла.
                         for (int i = 1; i <= pages; i++) { // Итерируем по страницам каждого файла.
                             String pageContent = PdfTextExtractor.getTextFromPage(doc.getPage(i)); // Получаем контент каждой страницы файла.
-                            words = pageContent.split("\\P{IsAlphabetic}+"); // Делим собранный контент на отдельные слова и собираем в массив.
+                            words = pageContent.split("\\P{IsAlphabetic}+"); // Делим собранный текст на отдельные слова и собираем в массив.
                             for (String word : words) {
                                 if (word.isEmpty()) {
                                     continue;
@@ -31,14 +31,30 @@ public class BooleanSearchEngine implements SearchEngine {
                                 word = word.toLowerCase();
                                 frequency.put(word, frequency.getOrDefault(word, 0) + 1); // Собираем промежуточный Map из слов и их количества в пределах каждого файла.
                             }
+//                            for (Map.Entry<String, Integer> iteration : frequency.entrySet()) {
+//                                String keyWord = iteration.getKey(); // Для каждой пары промежуточного Map "ключ (слово)- значение (количество)" извлекаем ключ.
+//                                if (!result.containsKey(keyWord)) { // Если результирующий Map поиска не содержит ключ промежуточного Map, заполняем новую пару "Слово - PageEntry"
+//                                    List<PageEntry> newEntry = new ArrayList<>();
+//                                    newEntry.add(new PageEntry(doc.getDocumentInfo().getTitle(), i, iteration.getValue()));
+//                                    result.put(keyWord, newEntry);
+//                                } else { // Если ключ промежуточного Map встретился в результирующем Map поиска (по результатам сканирования предыдущих файлов или страниц), обновляем List<PageEntry> для ключа.
+//                                    result.get(keyWord).add(new PageEntry(doc.getDocumentInfo().getTitle(), i, iteration.getValue()));
+//                                }
+//                            }
                             for (Map.Entry<String, Integer> iteration : frequency.entrySet()) {
+                                Map<String, Integer> countFor = new HashMap<>();
+                                int counter; // Общий счетчик для слов, повторяющихся в пределах папки-источника. Для выполнения дополнительного задания.
                                 String keyWord = iteration.getKey(); // Для каждой пары промежуточного Map "ключ (слово)- значение (количество)" извлекаем ключ.
                                 if (!result.containsKey(keyWord)) { // Если результирующий Map поиска не содержит ключ промежуточного Map, заполняем новую пару "Слово - PageEntry"
                                     List<PageEntry> newEntry = new ArrayList<>();
                                     newEntry.add(new PageEntry(doc.getDocumentInfo().getTitle(), i, iteration.getValue()));
                                     result.put(keyWord, newEntry);
+                                    counter = 1;
+                                    countFor.put(iteration.getKey(), counter);
                                 } else { // Если ключ промежуточного Map встретился в результирующем Map поиска (по результатам сканирования предыдущих файлов или страниц), обновляем List<PageEntry> для ключа.
                                     result.get(keyWord).add(new PageEntry(doc.getDocumentInfo().getTitle(), i, iteration.getValue()));
+                                    counter = iteration.getValue() + 1;
+                                    countFor.put(iteration.getKey(), counter);
                                 }
                             }
                         }
@@ -51,24 +67,13 @@ public class BooleanSearchEngine implements SearchEngine {
         }
     }
 
-//    @Override
-//    public List<PageEntry> search(String word) {
-//        if (result.containsKey(word.toLowerCase())) {
-//            List<PageEntry> sort = result.get(word);
-//            return sort.stream().sorted(Comparator.naturalOrder()).collect(Collectors.toList());
-//        } else { // В случае отсутствия слова в результате поиска, возвращается значение по-умолчанию.
-//            List<PageEntry> basicList = new ArrayList<>();
-//            basicList.add(new PageEntry("Basic.pdf", 0, 0));
-//            return basicList;
-//        }
-//    }
-
     @Override
     public List<PageEntry> search(String text) {
         String[] splitText = text.split("\\P{IsAlphabetic}+");
         List<PageEntry> pageEntry = null;
         for(String word : splitText) {
             if (result.containsKey(word.toLowerCase())) {
+                // Добавить через методы List возможность заменить count на значение counter из countFor. Как объединить название файла в один вывод? Что делать со страницами - в массив?
                 pageEntry = result.get(word);
             } else { // В случае отсутствия слова в результате поиска, возвращается значение по-умолчанию.
                 pageEntry = new ArrayList<>();
